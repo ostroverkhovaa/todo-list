5aa05689be8576e9e0c327a3cd2bc8fc63d2618f
Anastasiia <skubakovaa@gmail.com>
2025-06-22 23:14:27 +0200

    register models in Django admin

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from catalog.models import Task, Tag
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    search_fields = ["content"]
+    list_filter = ["is_done", "tags"]
+
+
+admin.site.register(Tag)
